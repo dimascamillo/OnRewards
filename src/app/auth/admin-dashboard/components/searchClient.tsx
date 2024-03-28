603b02a27075749b57e6434598394bcc74fd0ac9
Dimas Camillo <dimascamillo@gmail.com>
2024-03-28 10:17:27 -0300

    c

--- src/app/auth/admin-dashboard/components/searchClient.tsx
+++ src/app/auth/admin-dashboard/components/searchClient.tsx
@@ -1,5 +1,8 @@
+import useFormatCNPJ from "@/app/hook/useFormatCnpj";
 import { api } from "@/app/lib/axios";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { parseCookies } from "nookies";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -10,6 +13,10 @@ const consultingClientFormSchema = z.object({
 type ConsultingClientFormSchema = z.infer<typeof consultingClientFormSchema>;
 
 export default function SearchClient() {
+  const formatCNPJ = useFormatCNPJ();
+
+  const [clientNotFound, setClientNotFound] = useState("");
+
   const {
     register,
     handleSubmit,
@@ -23,15 +30,26 @@ export default function SearchClient() {
   async function handleCreateClient(data: ConsultingClientFormSchema) {
     const { context } = data;
 
+    const cookies = parseCookies();
+    const authToken = cookies.token;
+
     try {
-      await api.get("/consulting/client", {
+      const response = await api.get("/consulting/client", {
         params: {
-          context,
+          search: context,
+        },
+        headers: {
+          Authorization: `Bearer ${authToken}`,
         },
       });
 
+      setClientNotFound("");
+
       reset();
     } catch (err: any) {
+      if (err.response && err.response.status === 404) {
+        setClientNotFound("CNPJ não encontrado.");
+      }
       console.error(err.message);
     }
   }
@@ -39,7 +57,7 @@ export default function SearchClient() {
     <form onSubmit={handleSubmit(handleCreateClient)}>
       <div className="flex flex-col my-10">
         <label className="mb-3" htmlFor="">
-          Digite o CNPJ ou Nome do Cliente
+          Digite o CNPJ do Cliente
         </label>
         <div>
           <input
@@ -47,6 +65,10 @@ export default function SearchClient() {
             type="text"
             placeholder="Digite apenas números"
             {...register("context")}
+            onChange={(e) => {
+              const formattedCNPJ = formatCNPJ(e.target.value);
+              setValue("context", formattedCNPJ);
+            }}
           />
           <button
             className="bg-yellow-brand-400 text-black w-32 h-11 hover:bg-yellow-brand-500 transition-all rounded-e-sm"
@@ -56,6 +78,7 @@ export default function SearchClient() {
           </button>
         </div>
       </div>
+      <span className="text-red-600 font-semibold">{clientNotFound}</span>
     </form>
   );
 }
